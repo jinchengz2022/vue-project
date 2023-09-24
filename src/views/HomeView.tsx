eb413a0d3c8d98055c432cd9dd1d1029c30f5a13
!!false <jinchengz202011@163.com>
2023-09-24 22:04:42 +0800

    fix: 1

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -27,20 +27,6 @@ export const HomeView = defineComponent({
       }
     })
 
-    function currying(fn) {
-      let args = []
-      return function temp(...newArgs) {
-        if (newArgs.length) {
-          args = [...args, ...newArgs]
-          return temp
-        } else {
-          let val = fn.apply(this, args)
-          args = [] //保证再次调用时清空
-          return val
-        }
-      }
-    }
-
     return () => (
       <div class={s.wrapper}>
         <header>
